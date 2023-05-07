c3b83d19b4e314ffe3f33dda5c71bd1d98fce3ba
Andrew <andrew.dragoslavic@gmail.com>
2023-05-07 18:55:43 -0400

    New Code

---
+++ calc.py
@@ -0,0 +1,23 @@
+import math
+import random as r
+
+def add(x,y):
+    return x + y
+
+def sub(x,y):
+    return x - y
+
+def mult(x,y):
+    return x*y
+
+def div(x,y):
+    if(y == 0):
+        return('ERROR')
+    else:
+        return (x/y)
+
+x = input("x: ") 
+y = input("y: ")
+x = float(x)
+y = float(y)
+print (f'{x} / {y} = {div(x,y)}')
